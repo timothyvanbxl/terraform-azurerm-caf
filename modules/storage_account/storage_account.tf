aa5b083c031c360fe09b026c8271dd9ff86fb466
timothyvanbxl <89788668+timothyvanbxl@users.noreply.github.com>
2021-09-06 16:31:39 +0200

    Update storage_account.tf

--- modules/storage_account/storage_account.tf
+++ modules/storage_account/storage_account.tf
@@ -8,7 +8,7 @@ resource "azurecaf_name" "stg" {
   name          = var.storage_account.name
   resource_type = "azurerm_storage_account"
   prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
+  random_length = 4 #var.global_settings.random_length
   clean_input   = true
   passthrough   = var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
