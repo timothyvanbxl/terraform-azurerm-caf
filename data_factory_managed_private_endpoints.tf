9f6e232a73c6388d27ab0a7e82443746e2f88790
timothyvanbxl <89788668+timothyvanbxl@users.noreply.github.com>
2021-09-07 20:21:12 +0200

    Update data_factory_managed_private_endpoints.tf

--- data_factory_managed_private_endpoints.tf
+++ data_factory_managed_private_endpoints.tf
@@ -8,8 +8,23 @@ module "managed_private_endpoint_data_lake_storage_gen2" {
   data_factory_id    = module.data_factory[each.value.data_factory_key].id
   target_resource_id = module.storage_accounts[each.value.adls_storage_account_key].id
 }
-/*
-output "data_factory_dataset_azure_blob" {
-  value = module.data_factory_dataset_azure_blob
+
+module "managed_private_endpoint_keyvault" {
+  source = "./modules/data_factory/managed_private_endpoints/keyvault"
+
+  for_each = local.data_factory.managed_private_endpoints.keyvaults
+
+  name               = each.value.name
+  data_factory_id    = module.data_factory[each.value.data_factory_key].id
+  target_resource_id = module.keyvaults[each.value.keyvault_key].id
 }
-*/
+
+module "managed_private_endpoint_synapse_sql_pools" {
+  source = "./modules/data_factory/managed_private_endpoints/synapse_sql_pools"
+
+  for_each = local.data_factory.managed_private_endpoints.synapse_sql_pools
+
+  name               = each.value.name
+  data_factory_id    = module.data_factory[each.value.data_factory_key].id
+  target_resource_id = module.synapse_workspaces[each.value.synapse_workspaces_key].sql_pool.id
+}
