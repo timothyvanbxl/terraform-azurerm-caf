b2dd2e20f85d68293b6c39aacee43a2888a1d62b
timothyvanbxl <89788668+timothyvanbxl@users.noreply.github.com>
2021-09-09 11:27:59 +0200

    Update vmss_linux.tf

--- modules/compute/virtual_machine_scale_set/vmss_linux.tf
+++ modules/compute/virtual_machine_scale_set/vmss_linux.tf
@@ -14,7 +14,7 @@ resource "azurecaf_name" "linux" {
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
-  passthrough   = var.global_settings.passthrough
+  passthrough   = true #var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
 }
 
