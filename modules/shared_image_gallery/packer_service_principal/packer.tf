f7ae57b6d5caf6f81670075e0bd6e260d9f747ed
timothyvanbxl <89788668+timothyvanbxl@users.noreply.github.com>
2021-09-14 15:59:33 +0200

    Update packer.tf

--- modules/shared_image_gallery/packer_service_principal/packer.tf
+++ modules/shared_image_gallery/packer_service_principal/packer.tf
@@ -8,9 +8,7 @@ data "azurerm_key_vault_secret" "packer_secret" {
   key_vault_id = var.key_vault_id
 }
 
-
 data "template_file" "packer_template" {
-  template = file(var.settings.packer_template_filepath)
   vars = {
     client_id                         = data.azurerm_key_vault_secret.packer_client_id.value
     client_secret                     = data.azurerm_key_vault_secret.packer_secret.value
@@ -25,9 +23,7 @@ data "template_file" "packer_template" {
     managed_image_resource_group_name = var.resource_group_name
 
     managed_image_name    = var.settings.managed_image_name
-    ansible_playbook_path = var.settings.ansible_playbook_path
 
-    //shared_image_gallery destination values. If publishing to a different Subscription, change the following arguments and supply the values as variables
     subscription        = var.subscription
     resource_group      = var.resource_group_name
     gallery_name        = var.gallery_name
@@ -36,22 +32,12 @@ data "template_file" "packer_template" {
     replication_regions = var.settings.shared_image_gallery_destination.replication_regions
   }
 }
-
-resource "null_resource" "packer_configuration_generator" {
+resource "null_resource" "create_image" {
   provisioner "local-exec" {
-    command = "cat > ${var.settings.packer_config_filepath} <<EOL\n${data.template_file.packer_template.rendered}\nEOL"
+    command = "packer build -force ${var.settings.packer_config_filepath}"
   }
   depends_on = [
     data.template_file.packer_template
-  ]
-}
-
-resource "null_resource" "create_image" {
-  provisioner "local-exec" {
-    command = "packer build ${var.settings.packer_config_filepath}"
-  }
-  depends_on = [
-    null_resource.packer_configuration_generator
   ]
 }
 
@@ -67,23 +53,4 @@ data "azurerm_shared_image_version" "image_version" {
 
 resource "time_sleep" "time_delay_3" {
   destroy_duration = "60s"
-}
-
-resource "null_resource" "delete_image" {
-  triggers = {
-    resource_id = data.azurerm_shared_image_version.image_version.id
-  }
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = ["/bin/bash"]
-    command     = format("%s/destroy_image.sh", path.module)
-    on_failure  = fail
-    environment = {
-      RESOURCE_IDS = self.triggers.resource_id
-    }
-  }
-  depends_on = [
-    time_sleep.time_delay_3
-  ]
-
 }
