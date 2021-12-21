b91f3dc5d267063d22fd22fb8a9fb0a938564e26
timothyvanbxl <89788668+timothyvanbxl@users.noreply.github.com>
2021-12-21 15:18:49 +0100

    Add files via upload

--- data_factory_linked_services.tf
+++ data_factory_linked_services.tf
@@ -18,3 +18,25 @@ module "data_factory_linked_service_azure_blob_storage" {
 output "data_factory_linked_service_azure_blob_storage" {
   value = module.data_factory_linked_service_azure_blob_storage
 }
+
+##### azurerm_data_factory_linked_service_odata
+module "data_factory_linked_service_odata" {
+  source = "./modules/data_factory/linked_services/odata"
+
+  for_each = local.data_factory.linked_services.odata
+
+  name                     = each.value.name
+  resource_group_name      = local.resource_groups[each.value.resource_group_key].name
+  data_factory_name        = module.data_factory[each.value.data_factory_key].name
+  url                      = each.value.url
+  description              = try(each.value.description, null)
+  integration_runtime_name = try(each.value.integration_runtime_name, null)
+  annotations              = try(each.value.annotations, null)
+  parameters               = try(each.value.parameters, null)
+  additional_properties    = try(each.value.additional_properties, null)
+  basic_authentication     = try(each.value.basic_authentication, null)
+}
+
+output "data_factory_linked_service_odata" {
+  value = module.data_factory_linked_service_odata
+}
